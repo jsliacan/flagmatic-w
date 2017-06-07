5fd373d4b728afe8b05f5df92be93a8506a0f109
srobik <jakub.sliacan@gmail.com>
2017-06-07 17:38:14 +0100

    placed all certificates here

---
+++ certificates/k2k2k1_stab.js
@@ -0,0 +1,68 @@
+{
+    "admissible_graphs": [
+        "6:"
+    ], 
+    "description": "2-graph; maximize 5:1213141523243545 density; forbid induced 2:12", 
+    "number_of_admissible_graphs": 1, 
+    "bound": 0, 
+    "types": [
+        "0:", 
+        "2:", 
+        "4:"
+    ], 
+    "number_of_types": 3, 
+    "admissible_graph_densities": [
+        0
+    ], 
+    "numbers_of_flags": [
+        1, 
+        1, 
+        1
+    ], 
+    "flags": [
+        [
+            "3:"
+        ], 
+        [
+            "4:(2)"
+        ], 
+        [
+            "5:(4)"
+        ]
+    ], 
+    "order_of_admissible_graphs": 6, 
+    "qdash_matrices": [
+        [
+            [
+                1
+            ]
+        ], 
+        [
+            [
+                1
+            ]
+        ], 
+        [
+            [
+                1
+            ]
+        ]
+    ], 
+    "r_matrices": [
+        [
+            [
+                0
+            ]
+        ], 
+        [
+            [
+                0
+            ]
+        ], 
+        [
+            [
+                0
+            ]
+        ]
+    ]
+}
