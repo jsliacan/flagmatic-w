7ee752715ed9badf69cd9359dc7a0a5639d5dba9
Christoph Spiegel <spiegel@zib.de>
2022-02-22 08:58:01 +0100

    bug fix

--- pkg/flagmatic/problem.py
+++ pkg/flagmatic/problem.py
@@ -1491,7 +1491,9 @@ class Problem(SageObject):
         for ti in range(num_types):
             arguments.append( (self._types[ti], self._flags[ti], self._n, self._flag_cls, self._graphs) )
         
-        p = mp.Pool(min(48, int(2/3*mp.cpu_count())))
+        print("Using "+str(min(48, int(2/3*mp.cpu_count())+" out of "+str(mp.cpu_count())+" cores")
+        
+        p = mp.Pool(max(1, min(48, int(2/3*mp.cpu_count()))))
         for rarray in p.map(process_products, tqdm(arguments)):
             self._product_densities_arrays.append(rarray)
         p.close()
