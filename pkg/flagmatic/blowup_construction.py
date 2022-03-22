41c8acd8774eb9d265322d22c2a78097ae2a54b8
Christoph Spiegel <spiegel@zib.de>
2022-03-22 12:40:31 +0100

    added mp to subgraph_densities

--- pkg/flagmatic/blowup_construction.py
+++ pkg/flagmatic/blowup_construction.py
@@ -43,6 +43,29 @@ from .oriented_graph_flag import *
 from .construction import *
 
 
+def subgraph_densities_mp(P, n, cn, weights, graph, phantom_edge):
+    factor = factorial(n)
+    for i in range(1, cn + 1):
+        factor /= factorial(P.count(i))
+    if self._weights:
+        for v in P:
+            factor *= weights[v - 1]
+    ig = graph.degenerate_induced_subgraph(P)
+    igc = copy(ig)  # copy for phantom edge
+    ig.make_minimal_isomorph()
+    ghash = hash(ig)
+    
+    contains_phantom_edge = False
+    if phantom_edge is not None and all(x in P for x in phantom_edge):
+        contains_phantom_edge = True
+        phantom_edge = [P.index(x) + 1 for x in phantom_edge]
+        igc.add_edge(phantom_edge)
+        igc.make_minimal_isomorph()
+        gchash = hash(igc)
+    
+    return ghash, ig, gchash, igc, factor, contains_phantom_edge
+
+    
 class BlowupConstruction(Construction):
 
     def __init__(self, g, weights=None, field=None, phantom_edge=None, no_symmetry=False):
@@ -104,40 +127,56 @@ class BlowupConstruction(Construction):
         total = Integer(0)
         sharp_graph_counts = {}
         sharp_graphs = []
-
-        for P in UnorderedTuples(range(1, cn + 1), n):
-
-            factor = factorial(n)
-            for i in range(1, cn + 1):
-                factor /= factorial(P.count(i))
-
-            if self._weights:
-                for v in P:
-                    factor *= self._weights[v - 1]
-
-            ig = self._graph.degenerate_induced_subgraph(P)
-            igc = copy(ig)  # copy for phantom edge
-            ig.make_minimal_isomorph()
-
-            ghash = hash(ig)
+        
+        import multiprocessing as mp
+        p = mp.Pool()
+        for ghash, ig, gchash, igc, factor, contains_phantom_edge in p.starmap(subgraph_densities_mp, [(P, n, cn, self._weights, self._graph, self._phantom_edge if hasattr(self, "_phantom_edge") else None) for P in UnorderedTuples(range(1, cn + 1), n)]):
             if ghash in sharp_graph_counts:
                 sharp_graph_counts[ghash] += factor
             else:
                 sharp_graphs.append(ig)
                 sharp_graph_counts[ghash] = factor
-
             total += factor
-
-            if hasattr(self, "_phantom_edge") and all(x in P for x in self._phantom_edge):
-                phantom_edge = [P.index(x) + 1 for x in self._phantom_edge]
-                igc.add_edge(phantom_edge)
-                igc.make_minimal_isomorph()
-
-                ghash = hash(igc)
-                if not ghash in sharp_graph_counts:
+            
+            if contains_phantom_edge:
+                if not gchash in sharp_graph_counts:
                     sharp_graphs.append(igc)
-                    sharp_graph_counts[ghash] = Integer(0)
+                    sharp_graph_counts[gchash] = Integer(0)
+        p.close()
 
+        # for P in UnorderedTuples(range(1, cn + 1), n):
+        # 
+        #     factor = factorial(n)
+        #     for i in range(1, cn + 1):
+        #         factor /= factorial(P.count(i))
+        # 
+        #     if self._weights:
+        #         for v in P:
+        #             factor *= self._weights[v - 1]
+        # 
+        #     ig = self._graph.degenerate_induced_subgraph(P)
+        #     igc = copy(ig)  # copy for phantom edge
+        #     ig.make_minimal_isomorph()
+        # 
+        #     ghash = hash(ig)
+        #     if ghash in sharp_graph_counts:
+        #         sharp_graph_counts[ghash] += factor
+        #     else:
+        #         sharp_graphs.append(ig)
+        #         sharp_graph_counts[ghash] = factor
+        # 
+        #     total += factor
+        # 
+        #     if hasattr(self, "_phantom_edge") and all(x in P for x in self._phantom_edge):
+        #         phantom_edge = [P.index(x) + 1 for x in self._phantom_edge]
+        #         igc.add_edge(phantom_edge)
+        #         igc.make_minimal_isomorph()
+        # 
+        #         ghash = hash(igc)
+        #         if not ghash in sharp_graph_counts:
+        #             sharp_graphs.append(igc)
+        #             sharp_graph_counts[ghash] = Integer(0)
+        
         return [(g, sharp_graph_counts[hash(g)] / total) for g in sharp_graphs]
 
     def zero_eigenvectors(self, tg, flags):
