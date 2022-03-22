12e10453c31df4296edfaa56ea1357b657bf236e
Christoph Spiegel <spiegel@zib.de>
2022-03-22 10:37:35 +0100

    test

--- pkg/flagmatic/hypergraph_flag.pyx
+++ pkg/flagmatic/hypergraph_flag.pyx
@@ -602,6 +602,7 @@ cdef class HypergraphFlag (Flag):
                     for sg in (tqdm(smaller_graphs) if show_progress else smaller_graphs):
                         graph_list, hash_list = process_small_graphs_mp((sg, n, s, max_ne, possible_edges, oriented, forbidden_edge_numbers, forbidden_graphs, forbidden_induced_graphs))
                         for ng, ng_hash in zip(graph_list, hash_list):
+                            print(".")
                             if not ng_hash in hashes:
                                 new_graphs.append(ng)
                                 hashes.add(ng_hash)
